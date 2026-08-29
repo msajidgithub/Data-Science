8476a979858d84e3c20416998a14b18ddb0892b9
m.sajid <m.sajid@enabill.com>
2026-08-29 10:47:05 +0500

    dd

---
+++ ai-engineering/python/leasson-01/practice.py
@@ -0,0 +1,5 @@
+age = 20
+if age >= 19:
+    print("You are an adult.")
+else:
+    print("You are not an adult.")
---
+++ hello.py
@@ -0,0 +1,6 @@
+name = "Sajid"
+age = 30
+
+print("Hello, World!")
+print("My name is", name)
+print("My age is", age)

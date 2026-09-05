04ba4e38e6a649ba6cdbad058818ae8938a5d5cd
m.sajid <m.sajid@enabill.com>
2026-09-05 13:35:35 +0500

    added

--- class27/app.py
+++ class27/app.py
@@ -15,3 +15,17 @@ def clean_text(text):
     return text
 
 st.title("Sentiment Analysis App")
+review = st.text_area("Enter your review here:")
+
+if st.button("Predict Sentiment"):
+    if review.strip() == '':
+        st.warning("Please enter a review before predicting.")
+    else:
+        cleaned_review = clean_text(review)
+        review_vectorized = tfidf.transform([cleaned_review])
+        prediction = model.predict(  )[0]
+
+        if prediction == 'Positive':
+            st.success("The sentiment of the review is Positive.")
+        else:
+            st.error("The sentiment of the review is Negative.")
